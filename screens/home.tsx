ead430d6b97a16d13d8d231a68655a4b3b6a8e59
Eduardo Go <egomatsushita@gmail.com>
2021-12-29 17:42:45 -0500

    [BZ-1] Add custom fonts

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,9 +1,16 @@
 import { StyleSheet, View, Text } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function Home() {
+  const [loaded] = useFonts({
+    nunito: require("../assets/fonts/Nunito-Regular.ttf"),
+    nunitoBold: require("../assets/fonts/Nunito-Bold.ttf"),
+  });
+  if (!loaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.text}>Home</Text>
     </View>
   );
 }
@@ -14,5 +21,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#ddd",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontFamily: "nunito",
   },
 });
